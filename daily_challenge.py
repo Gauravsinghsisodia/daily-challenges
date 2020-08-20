f549f494273e294b89d3b797a174382e07f87ebf
GauravsinghSisodia <65955464+Gauravsinghsisodia@users.noreply.github.com>
2020-08-20 23:20:32 +0530

    Add files via upload

---
+++ daily_challenge.py
@@ -0,0 +1,63 @@
+a=[]
+b=[]
+c=[]
+for x in range(3):
+    print("Enter/Paste your prompt. Ctrl-D to save it.")
+    contents = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        contents.append(line)
+    question1='\n'.join(contents)
+    name=input("Input function name")
+    n=int(input("Enter no. of test cases:\n"))
+    inp=[input("Enter inputs:\n") for _ in range(n)]
+    out=[input("Enter outputs:\n") for _ in range(n)]
+    notes=input("Enter Notes:\n")
+    var=f'*{notes}*\n\n'
+    if x==0:
+        a.append("**Here Is Today's :python: Task for Beginners:**")
+        a.append('------------------------------------------------')
+        a.append(question1+'\n')
+        a.append("**Examples**")
+        a.append("```python")
+        for x,y in zip(inp,out):
+            a.append(f"{name}({x}) ➞ {y}\n")
+        a.append("```\n\n")
+        a.append("**Notes:**")
+        a.append(var)
+    elif x==1:
+        b.append("**Here Is Today's :python: Task for Intermediates:**")
+        b.append('------------------------------------------------')
+        b.append(question1+'\n')
+        b.append("**Examples**")
+        b.append("```python")
+        for x,y in zip(inp,out):
+            b.append(f"{name}({x}) ➞ {y}\n")
+        b.append("```\n\n")
+        b.append("**Notes:**")
+        b.append(var)
+    elif x==2:
+        c.append("**Here Is Today's :python: Task for Experts:**")
+        c.append('------------------------------------------------')
+        c.append(question1+'\n')
+        c.append("**Examples**")
+        c.append("```python")
+        for x,y in zip(inp,out):
+            c.append(f"{name}({x}) ➞ {y}\n")
+        c.append("```\n\n")
+        c.append("**Notes:**")
+        c.append(var)
+print("@Coders")
+print('------------------------------------------------')
+for x in a:
+    print(x)
+for x in b:
+    print(x)
+for x in c:
+    print(x)
+print("---------------------------")
+print("**Goodluck to you all :v:**")
+print("---------------------------")
